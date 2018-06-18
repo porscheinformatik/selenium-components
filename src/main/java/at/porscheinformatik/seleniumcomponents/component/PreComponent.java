6eac96c1b93bf73510470a85fca7358f8a416141
Daniel Furtlehner <daniel.furtlehner@porscheinformatik.at>
2018-06-18 09:01:24 +0200

    Adds PreComponent

---
+++ src/main/java/at/porscheinformatik/seleniumcomponents/component/PreComponent.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package at.porscheinformatik.seleniumcomponents.component;
+
+import static at.porscheinformatik.seleniumcomponents.WebElementSelector.*;
+
+import at.porscheinformatik.seleniumcomponents.AbstractSeleniumComponent;
+import at.porscheinformatik.seleniumcomponents.SeleniumComponent;
+import at.porscheinformatik.seleniumcomponents.WebElementSelector;
+
+/**
+ * @author Daniel Furtlehner
+ */
+public class PreComponent extends AbstractSeleniumComponent
+{
+
+    public PreComponent(SeleniumComponent parent)
+    {
+        super(parent, selectByTagName("pre"));
+    }
+
+    public PreComponent(SeleniumComponent parent, WebElementSelector selector)
+    {
+        super(parent, selector);
+    }
+
+    public String getContent()
+    {
+        return getText();
+    }
+}
