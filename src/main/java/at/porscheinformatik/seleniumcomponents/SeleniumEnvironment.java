e85643923ba896d38fec52e3cd1b7243e8913042
Daniel Furtlehner <daniel.furtlehner@porscheinformatik.at>
2019-09-26 09:50:27 +0200

    Add date handling to SeleniumEnvironment
    This makes it easier to convert dates from and to the language used
    in the website.

--- src/main/java/at/porscheinformatik/seleniumcomponents/SeleniumEnvironment.java
+++ src/main/java/at/porscheinformatik/seleniumcomponents/SeleniumEnvironment.java
@@ -1,5 +1,8 @@
 package at.porscheinformatik.seleniumcomponents;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Locale;
 
 import org.openqa.selenium.WebDriver;
@@ -25,6 +28,73 @@ public interface SeleniumEnvironment
      * @return the locale
      */
     Locale getLanguage();
+
+    /**
+     * Parses the given date string in the locale returned by {@link #getLanguage()} with {@link FormatStyle#MEDIUM}
+     * 
+     * @param dateAsString the date to parse
+     * @return the parsed date
+     */
+    default LocalDate parseDate(String dateAsString)
+    {
+        return parseDate(dateAsString, FormatStyle.MEDIUM);
+    }
+
+    /**
+     * Parses the given date string in the locale returned by {@link #getLanguage()}
+     * 
+     * @param dateAsString the date to parse
+     * @param style the date style
+     * @return the parsed date
+     */
+    default LocalDate parseDate(String dateAsString, FormatStyle style)
+    {
+        if (dateAsString == null)
+        {
+            return null;
+        }
+
+        DateTimeFormatter formatter = getDateTimeFormatter(style);
+
+        return LocalDate.parse(dateAsString, formatter);
+    }
+
+    /**
+     * Formats the given date string in the locale returned by {@link #getLanguage()} with {@link FormatStyle#MEDIUM}
+     * 
+     * @param date the date to format
+     * @return the formatted date
+     */
+    default String formatDate(LocalDate date)
+    {
+        return formatDate(date, FormatStyle.MEDIUM);
+    }
+
+    /**
+     * Formats the given date string in the locale returned by {@link #getLanguage()}
+     * 
+     * @param date the date to format
+     * @param style the date style
+     * @return the formatted date
+     */
+    default String formatDate(LocalDate date, FormatStyle style)
+    {
+        if (date == null)
+        {
+            return null;
+        }
+
+        DateTimeFormatter formatter = getDateTimeFormatter(style);
+
+        return date.format(formatter);
+    }
+
+    default DateTimeFormatter getDateTimeFormatter(FormatStyle style)
+    {
+        Locale language = getLanguage();
+
+        return DateTimeFormatter.ofLocalizedDate(style).localizedBy(language);
+    }
 
     /**
      * Returns the message with the specified key. If the message is missing, it should return null.
