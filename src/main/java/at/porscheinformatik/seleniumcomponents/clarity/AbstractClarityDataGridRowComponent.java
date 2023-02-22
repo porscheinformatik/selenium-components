d583109ce4af17e9e9a3999c767ab2a078e086f8
Carolin Schuntermann <carolin.schuntermann@porscheinformatik.com>
2023-02-22 13:25:28 +0100

    Expand Clarity DataGrid components
    Also introduced an icon direction enum.

--- src/main/java/at/porscheinformatik/seleniumcomponents/clarity/AbstractClarityDataGridRowComponent.java
+++ src/main/java/at/porscheinformatik/seleniumcomponents/clarity/AbstractClarityDataGridRowComponent.java
@@ -11,6 +11,8 @@ import at.porscheinformatik.seleniumcomponents.AbstractSeleniumComponent;
 import at.porscheinformatik.seleniumcomponents.SeleniumComponent;
 import at.porscheinformatik.seleniumcomponents.SeleniumComponentListFactory;
 import at.porscheinformatik.seleniumcomponents.WebElementSelector;
+import at.porscheinformatik.seleniumcomponents.clarity.ClarityIconComponent.ClarityIconDirection;
+import at.porscheinformatik.seleniumcomponents.component.ButtonComponent;
 import at.porscheinformatik.seleniumcomponents.component.HtmlComponent;
 
 /**
@@ -19,6 +21,13 @@ import at.porscheinformatik.seleniumcomponents.component.HtmlComponent;
 public abstract class AbstractClarityDataGridRowComponent extends AbstractSeleniumComponent
 {
     protected final SeleniumComponentListFactory<HtmlComponent> cells;
+    protected final HtmlComponent rowDetail = new HtmlComponent(this, selectByTagName("clr-dg-row-detail"));
+
+    private final ButtonComponent expandButton =
+        new ButtonComponent(this, selectByClassName("datagrid-expandable-caret-button"));
+    private final ClarityIconComponent caret = ClarityIconComponent.in(expandButton);
+
+    // ---
 
     public AbstractClarityDataGridRowComponent(SeleniumComponent parent, WebElementSelector selector)
     {
@@ -27,11 +36,38 @@ public abstract class AbstractClarityDataGridRowComponent extends AbstractSeleni
         cells = SeleniumComponentListFactory.of(this, selectByClassName("datagrid-cell"), HtmlComponent::new);
     }
 
+    // ---
+
+    public boolean isCollapsed()
+    {
+        return caret.getDirection() == ClarityIconDirection.RIGHT;
+    }
+
+    public boolean isExpanded()
+    {
+        return caret.getDirection() == ClarityIconDirection.DOWN;
+    }
+
+    public void collapse()
+    {
+        if (isExpanded())
+        {
+            expandButton.click();
+        }
+    }
+
+    public void expand()
+    {
+        if (isCollapsed())
+        {
+            expandButton.click();
+        }
+    }
+
     protected <T> T mapCell(int index, Function<HtmlComponent, T> mapping)
     {
         HtmlComponent cell = cells.findAll().get(index);
 
         return mapping.apply(cell);
     }
-
 }
